9fcba1fc75d60fce01ab1c3c022a0bddf14d0dc5
brock <54557110+BrockCruess@users.noreply.github.com>
2024-02-06 20:07:40 -0500

    Create iag-cli-failcatcher.sh

---
+++ CLI Fail Catcher/iag-cli-failcatcher.sh
@@ -0,0 +1,32 @@
+# Don't forget to make this .sh file executable with "chmod +x iag-cli-updater.sh"
+# Set up a cronjob to run this script every hour with "crontab -e"
+#!/bin/bash
+
+uname=$(uname)
+
+if test "$uname" = "FreeBSD"
+then os=freebsd
+else
+if test "$uname" = "Linux"
+then os=linux
+else
+if test "$uname" = "Darwin"
+then os=macos
+fi
+fi
+fi
+
+cd "$(dirname "$0")"
+iagstatus=$(./iag-cli-$os get:status 2>&1)
+
+if [[ $iagstatus == *"up"* ]]; then
+echo "Iagon Node is running normally!"
+else
+if [[ $iagstatus == *"not"* ]]; then
+echo "Iagon Node is not running, restarting it now..."
+./iag-cli-$os stop && ./iag-cli-$os start
+else
+echo "An unknown error occured, please manually kill the Iagon Node process and restart it. Open a support ticket in Iagon's Discord for more help."
+fi
+fi
+fi
