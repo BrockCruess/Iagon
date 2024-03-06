5d693bd5763dd9e8896bbc46c5a661631d7bc4ef
brock <54557110+BrockCruess@users.noreply.github.com>
2024-03-06 09:14:47 -0500

    Add process kill and restart for error "Node maybe already running, please check"

--- CLI Fail Catcher/iag-cli-failcatcher.sh
+++ CLI Fail Catcher/iag-cli-failcatcher.sh
@@ -21,14 +21,26 @@ iagstatus=$(./iag-cli-$os get:status 2>&1)
 
 if [[ $iagstatus == *"up"* ]]; then
 echo "Iagon Node is running normally!"
-else
-if [[ $iagstatus == *"not"* ]]; then
+elif [[ $iagstatus == *"not"* ]]; then
 echo "Iagon Node is not running, restarting it now..."
 ./iag-cli-$os stop && ./iag-cli-$os start
 time=$(date)
 echo "$time: Iagon Storage Node was not running, so it was restarted." >> failures.log && \
 echo " " >> failures.log
+elif [[ $iagstatus == *"check"* ]]; then
+echo "The Iagon Node process is frozen, killing and restarting it now..."
+tmpfile=$(mktemp processes.tmp.XXXXXX)
+ps -aux | sed -E 's/ +/,/g' > $tmpfile
+pid1=$(cat $tmpfile | awk '/iagon/' | sed -n 1p | awk -F "\"*,\"*" '{print $2}')
+pid2=$(cat $tmpfile | awk '/iagon/' | sed -n 2p | awk -F "\"*,\"*" '{print $2}')
+pid3=$(cat $tmpfile | awk '/iagon/' | sed -n 3p | awk -F "\"*,\"*" '{print $2}')
+pid4=$(cat $tmpfile | awk '/iagon/' | sed -n 4p | awk -F "\"*,\"*" '{print $2}')
+rm "$tmpfile"
+kill $pid1
+kill $pid2
+kill $pid3
+kill $pid4
+./iag-cli-$os start
 else
 echo "An unknown error occured, please manually kill the Iagon Node process and restart it. Open a support ticket in Iagon's Discord for more help."
-fi
 fi
